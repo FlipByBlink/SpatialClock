4b7e843c0399608361ce09ec1459e8dcee28e504
aaaa <a@a>
2024-01-15 15:21:59 +0900

    Initial Commit

---
+++ SpatialClock/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SpatialClock
+//
+//  Created by FULLNAME on 2024/01/15.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ SpatialClock/SpatialClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpatialClockApp.swift
+//  SpatialClock
+//
+//  Created by FULLNAME on 2024/01/15.
+//
+
+import SwiftUI
+
+@main
+struct SpatialClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
